160d4568074f2623551fbd48d695d62a0ff9d5d7
Allana Idalgo <allana.idalgo@wildlifestudios.com>
2025-09-17 23:18:31 -0300

    initial commit

---
+++ JPApexPredators/JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Allana Idalgo on 17/09/25.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
